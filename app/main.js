9b7436b4224b146fd2afd1678fb3b944d51c43a9
John Iball <johniball@yahoo.com>
2016-06-18 18:01:54 +0100

    Added simple stylesheet and hot module replacement in Webpack

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 import SearchComponent from './SearchComponent';
+import './main.css';
 
 render(<SearchComponent />, document.getElementById('root'));

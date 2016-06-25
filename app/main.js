c8d8c718152cf3441bcc833814e60214b8664846
John Iball <johniball@yahoo.com>
2016-06-25 20:25:34 +0100

    Moved search to its own directory

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import SearchComponent from './SearchComponent';
+import SearchComponent from './search/SearchComponent';
 import './main.css';
 
 render(<SearchComponent />, document.getElementById('root'));
